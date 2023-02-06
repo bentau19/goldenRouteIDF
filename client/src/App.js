634a0f840890813c49e085db4dd9a78ef3fb32c4
bentau19 <bentau19@gmail.com>
2023-02-06 11:48:00 +0200

    commands and server bug fixed

--- client/src/App.js
+++ client/src/App.js
@@ -7,50 +7,80 @@ function App() {
 const [opearentionable, setOpearentionable] = useState(false)
 const [inputValue, setInputValue] = useState("");
 const [message, setMessage] = useState("");
-const [isFirst, setFlag] = useState(false);
+const [isFirst, setFlag] = useState(true);
 const initialDate = new Date('2023-01-01');
 const [selectedDate, setSelectedDate] = useState(initialDate);
-const [dates, setDates] = useState("loading");
+const [hours, setHours] = useState("loading");
 
-useEffect(() => weatherCheck,[]);
+useEffect(() => weatherCheck,
+   // eslint-disable-next-line react-hooks/exhaustive-deps
+   []);
 
 const handleInputChange = (event) => {
     setInputValue(event.target.value);
 };
 
-const weatherCheck = async()=> {
-        if(isFirst===false){
-          setDates("loading")
-        const day = (selectedDate.getDate()).toString().padStart(2, '0');
-        const month = (selectedDate.getMonth() + 1).toString().padStart(2, '0');
-        const year = selectedDate.getFullYear().toString();
-        const response = await axios.post("http://localhost:5000//weather", { year:year, month:month,day:day});
-        var html = ''
-        const length =response.data.length
-        setOpearentionable(typeof(response.data[0])==="string")
-        for (var i=0;i<length;i++){
-            html+=response.data[i]
-            html+="\n"
-                }
-        if (dates === ""){
-          weatherCheck()
-        }else{
-          setFlag(true)
-        }
-        setDates(html)
+
+const weatherCheck = async(counter = 1) => {
+  // Check if the function is being executed only once
+  if (isFirst) {
+    // Update the hours variable to loading
+    setHours("loading");
+
+    // Get the date from selectedDate object
+    const day = (selectedDate.getDate()).toString().padStart(2, '0');
+    const month = (selectedDate.getMonth() + 1).toString().padStart(2, '0');
+    const year = selectedDate.getFullYear().toString();
+
+    // Make a POST request to the weather endpoint
+    const response = await axios.post("http://localhost:5000//weather", { year:year, month:month,day:day});
+    
+    // Initialize a variable to store the data in HTML format
+    var html = '';
+    const length = response.data.length;
+
+    // Update the state of operationable based on the data received
+    setOpearentionable(typeof(response.data[0]) === "string");
+
+    // Loop through the data received and store it in the HTML format
+    for (var i = 0; i < length; i++) {
+      html += response.data[i];
+      html += "\n";
     }
- }
+
+    // Check if hours is an empty string
+    if (hours === "" && counter !=3) {
+      // If it is, call the weatherCheck function again
+      weatherCheck(counter++);
+    } else {
+      // If not, update the state of isFirst to false
+      setFlag(false);
+    }
+
+    // Update the state of hours with the data in HTML format
+    setHours(html);
+  }
+}
 
 const handleSubmit = async (event) => {
-    event.preventDefault();
-    const response = await axios.post("http://localhost:5000//physics_calc", { inputData: inputValue });
-    setMessage(response.data);
+  // Prevent the default form submit behavior
+  event.preventDefault();
+  
+  // Send a post request to the server with the input value as the payload
+  const response = await axios.post("http://localhost:5000//physics_calc", { inputData: inputValue });
+  
+  // Update the state with the response data
+  setMessage(response.data);
+  };
+
+// This function is called whenever the date is changed
+const handleDateChange = (event) => {
+  // Set the selected date to the new date chosen by the user
+  setSelectedDate(new Date(event.target.value));
+  // Set the isfirst to true, indicating that the date has been changed
+  setFlag(true);
 };
 
-  const handleDateChange = (event) => {
-    setSelectedDate(new Date(event.target.value));
-    setFlag(false);
-  };
 
 return (
     <div className="App">
@@ -62,24 +92,47 @@ return (
         <button className="number-input__button" type="submit">שלח</button> 
     </form>
     {
-      message!==""?
-      message!==-9?
-      <div>
-        <h3>{message[0]} sec :זמן</h3>
-        <h3>{message[1]} m :מרחק מראה</h3>
-        <h3>{message[2]} kg :משקל להשמדה</h3></div>
-        :<h3> incorrect varible</h3>:<p></p>
+      message !== "" ? 
+        message !== -9 ? 
+          <div>
+            {/* Displaying the calculated time, distance and weight */}
+            <h3>{message[0]} sec :זמן</h3>
+            <h3>{message[1]} m :מרחק מראה</h3>
+            <h3>{message[2]} kg :משקל להשמדה</h3>
+          </div>
+        : 
+          <h3>incorrect variable</h3> 
+      : 
+        <p></p>
     }
+
     <hr></hr>
     <h3>LATITUDE = 30 : LONGITUDE = 35</h3>
     <h2>בחר/י את התאריך בו המבצע יבוצע</h2>
     <input type="date" value={selectedDate.toISOString().substr(0, 10)} onChange={handleDateChange} />
     <button onClick={ weatherCheck} >שלח</button> 
-    {dates[0]==="c"||dates[0]==="l"?<p>{dates}</p>:opearentionable?
-    <div><h2 className='Opearentionable_Text_Style'>:ניתן לקיים את המבצע בתאריך הנבחר בשעות</h2><h3 >{dates}</h3></div>:<div><h2 className='UnOpearentionable_Text_Style'>לא ניתן לקיים את המבצע בתאריך הנבחר
-      <br></br>
-      :והטמפרטורה באיזור ההמראה בתאריך זה היא</h2><h3 >{dates}c</h3></div>}
-      { <div className="star-of-david"></div> }
+
+
+    {      
+  // Conditional rendering to show the result of weather check
+    // Check if the first character of `hours` is 'c' or 'l' to show connection_errors or loading
+    hours[0] === "c" || hours[0] === "l" ? 
+        // If yes, render a paragraph with the contents of `hours`
+        <p>{hours}</p>
+        // If not, check if `opearentionable` is truthy
+        : opearentionable ? 
+            // If `opearentionable` is truthy, render a div with the text and the contents of `hours`
+            <div>
+                <h2 className='Opearentionable_Text_Style'>ניתן לקיים את המבצע בתאריך הנבחר בשעות</h2>
+                <h3>{hours}</h3>
+            </div>
+            // If `opearentionable` is falsy, render a div with the text and the contents of `hours` with a 'c' appended to show celsius
+            : <div>
+                <h2 className='UnOpearentionable_Text_Style'>לא ניתן לקיים את המבצע בתאריך הנבחר<br></br>:והטמפרטורה באיזור ההמראה בתאריך זה היא</h2>
+                <h3>{hours}c</h3>
+            </div>
+  }
+      <div className='star-of-david'></div>
       <span className="dot"></span> 
     </div>
 );
--- server/server.py
+++ server/server.py
@@ -29,72 +29,95 @@ class SearchResult(db.Model):
         self.take_off_time = take_off_time
 
 
-def requests_weather(url, counter):  # anti server spam handler
+
+# Function to handle weather request with anti-spam mechanism
+def requests_weather(url, counter=1):
+    # Return "error" if maximum retry attempts reached
     if counter == 8:
         return "error"
+
     try:
-        response = requests.get(url)
+        # Make API request and add timeout value with retry counter
+        response = requests.get(url, timeout=(0.25 + (counter * 0.25)))
         return response
-    except requests.exceptions.ConnectionError as e:
-        counter += 1
-        return requests_weather(url, counter)
+    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as e:
+        # Retry request on connection or read timeout error
+        return requests_weather(url, counter + 1)
     except Exception as g:
-        print(f"erorrr ", type(g), g)
         return "problem"
 
-
-# Flask server
+# Flask route to handle weather API request
 @app.route("/weather", methods=["POST"])
-def weather_api_sniffer():  # get the Opearentionable times or temp function
+def weather_api_sniffer():
     try:
+        # Get year, month and day from POST request
         year = request.json["year"]
         month = request.json["month"]
         day = request.json["day"]
-        print(f"{year}:{month}:{day}")
+        # Form URL for API request
         url = f'https://archive-api.open-meteo.com/v1/archive?latitude={LATITUDE}&longitude={LONGITUDE}&start_date={year}-{month}-{day}&end_date={year}-{month}-{day}&hourly=temperature_2m'
-        response = requests_weather(url, 1)
+
+        # Call function to make API request
+        response = requests_weather(url)
+        if type(response) == str:
+            # Return "connection error" if request fails
+            return "connection error"
         if response.status_code == 200:
             data = response.json()
             relevant_times = []
-            if len(data['hourly']['temperature_2m']) == len(data['hourly']['time']):  # if the data is reliable
-                for i in range(len(data['hourly']['temperature_2m'])):  # extract all Opearentionable times
-                    try:
-                        if MIN_TEMPERATURE <= data['hourly']['temperature_2m'][i] <= MAX_TEMPERATURE:
-                            relevant_times.append(data['hourly']['time'][i].split("T")[1])
-                    except:
-                        return "connection _Error"
-            print("finish")
-            if not relevant_times:  # if there is no times return the temp
+            # Check if both temperature and time data exists for each hour
+            if len(data['hourly']['temperature_2m']) == len(data['hourly']['time']):
+                for i in range(len(data['hourly']['temperature_2m'])):
+                    # Append relevant time if temperature is within specified range
+                    if MIN_TEMPERATURE <= data['hourly']['temperature_2m'][i] <= MAX_TEMPERATURE:
+                        relevant_times.append(data['hourly']['time'][i].split("T")[1])
+            # Return first temperature if no relevant times found
+            if not relevant_times:
                 return [data['hourly']['temperature_2m'][1]]
             return relevant_times
         else:
-            print('Failed to retrieve data from API')
-            return "connection _Error"
-    except():
+            # Return "connection error" if API request fails
+            return "connection error"
+    except:
+        # Return "error" for all other exceptions
         return "error"
-    return "error"
 
 
 def db_save(result, load_mass):
+    # function to save results to database
+
+    # check if result is not equal to error code
     if result != ERROR_CODE:
+        # create a SearchResult object with load_mass, take_off_time, take_off_distance, and overweight
         sr = SearchResult(load_mass, result[1], result[2], result[0])
+        # add the object to the database session
         db.session.add(sr)
+        # commit the session to save the changes
         db.session.commit()
     else:
+        # if result is equal to error code, print error message
         print("its wrong!!")
 
 
 @app.route("/physics_calc", methods=["POST"])
-def receive_input():  # main physics calc receive function
+def receive_input():
+    # main physics calculation receive function
+
+    # get the load_mass from the request JSON
     load_mass = request.json["inputData"]
+    # search for existing result in the database
     db_result = SearchResult.query.filter_by(load_mass=load_mass).first()
+    # if result is found in the database
     if db_result is not None:
+        # return the take_off_time, take_off_distance, and overweight
         return jsonify(db_result.take_off_time, db_result.take_off_distance,
                        db_result.overweight)
+    # if result is not found in the database
     result = physics_calc.general_calc(int(load_mass))
+    # save the result to the database
     db_save(result, load_mass)
+    # return the result
     return jsonify(result)
-
 
 if __name__ == "__main__":
     with app.app_context():
